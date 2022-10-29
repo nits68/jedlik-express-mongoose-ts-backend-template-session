976962278ac3635f3835ffe9dc19daf5932871d9
nitslaszlo <nitslaszlo@gmail.com>
2022-10-29 21:38:37 +0200

    with secure-false

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,8 +1,8 @@
-import SessionExpiredException from "../exceptions/SessionExpiredException";
+import SessionExpiredException from "./../exceptions/SessionExpiredException";
 import { NextFunction, Response } from "express";
-import ISession from "interfaces/session.interface";
-import IRequestWithUser from "../interfaces/requestWithUser.interface";
-import userModel from "../user/user.model";
+import ISession from "./../interfaces/session.interface";
+import IRequestWithUser from "./../interfaces/requestWithUser.interface";
+import userModel from "./../user/user.model";
 
 export default async function authMiddleware(req: IRequestWithUser, res: Response, next: NextFunction): Promise<void> {
     if (req.session.id && (req.session as ISession).user_id) {

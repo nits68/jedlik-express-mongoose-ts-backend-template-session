f99b2da36e7b860a9ed3eb761ea509224d61729e
nitslaszlo <nitslaszlo@gmail.com>
2022-10-29 20:52:48 +0200

    up

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { config } from "dotenv";
+import dotenv from "dotenv";
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
 import PostController from "./post/post.controller";
@@ -6,7 +6,7 @@ import ReportController from "./report/report.controller";
 import UserController from "./user/user.controller";
 import RecipeController from "./recipe/recipe.controller";
 
-config(); // Read and set variables from .env file.
+dotenv.config(); // Read and set variables from .env file.
 
 const app = new App([new PostController(), new AuthenticationController(), new UserController(), new ReportController(), new RecipeController()]);
 

66be8f420710203df18853d92b9ee18b31536ee3
nitslaszlo <nitslaszlo@gmail.com>
2022-10-30 00:55:42 +0200

    teszt 10000

--- src/app.ts
+++ src/app.ts
@@ -37,9 +37,9 @@ export default class App {
         this.app.use(
             cors({
                 origin: ["https://minimal-dialogs.netlify.app", "https://jedlik-vite-quasar-template.netlify.app", "https://jedlik-vite-ts-template.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"],
-                allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Content-Language", "Expires", "Last-Modified", "Pragma"],
+                // allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Content-Language", "Expires", "Last-Modified", "Pragma"],
                 credentials: true,
-                exposedHeaders: ["Set-Cookie"],
+                // exposedHeaders: ["Set-Cookie"],
             }),
         );
 

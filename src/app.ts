adf13cdf6379729030b8d8c82673631369326b2c
nitslaszlo <nitslaszlo@gmail.com>
2022-10-29 22:41:42 +0200

    add origin https://minimal-dialogs.netlify.app

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ export default class App {
         // Enabled CORS:
         this.app.use(
             cors({
-                origin: ["https://jedlik-vite-quasar-template.netlify.app", "https://jedlik-vite-ts-template.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"],
+                origin: ["https://minimal-dialogs.netlify.app", "https://jedlik-vite-quasar-template.netlify.app", "https://jedlik-vite-ts-template.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"],
                 allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Content-Language", "Expires", "Last-Modified", "Pragma"],
                 credentials: true,
                 exposedHeaders: ["Set-Cookie"],

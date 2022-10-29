cbcc646fe20610a0041221cc6d8882a74074068f
nitslaszlo <nitslaszlo@gmail.com>
2022-10-30 01:18:23 +0200

    up

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ export default class App {
                 origin: ["https://minimal-dialogs.netlify.app", "https://jedlik-vite-quasar-template.netlify.app", "https://jedlik-vite-ts-template.netlify.app", "http://localhost:8080", "http://127.0.0.1:8080"],
                 allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie", "Cache-Control", "Content-Language", "Expires", "Last-Modified", "Pragma"],
                 credentials: true,
-                // exposedHeaders: ["Set-Cookie"],
+                exposedHeaders: ["Set-Cookie"],
             }),
         );
 

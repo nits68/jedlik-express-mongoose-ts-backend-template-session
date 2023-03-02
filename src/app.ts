20921a05a8471ff6997798ba92ce660322949fb4
nitslaszlo <nitslaszlo@gmail.com>
2023-03-02 23:38:45 +0100

    upgrade mongoose 7.0.0

--- src/app.ts
+++ src/app.ts
@@ -118,11 +118,7 @@ export default class App {
         const { MONGO_URI, MONGO_DB } = process.env;
         // Connect to MongoDB Atlas, create database if not exist::
         mongoose.set("strictQuery", true); // for disable DeprecationWarning
-        mongoose.connect(MONGO_URI, { dbName: MONGO_DB }, err => {
-            if (err) {
-                console.log("Unable to connect to the server. Please start MongoDB.");
-            }
-        });
+        mongoose.connect(MONGO_URI, { dbName: MONGO_DB }).catch(error => console.log(`Mongoose error on connection! Message: ${error.message}`));
 
         mongoose.connection.on("error", error => {
             console.log(`Mongoose error message: ${error.message}`);

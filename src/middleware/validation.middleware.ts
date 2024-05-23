fde1b47692612f8348b260e8183d4b137b4b2703
nitslaszlo <nitslaszlo@gmail.com>
2024-05-23 20:47:10 +0200

    upgrade validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -4,29 +4,26 @@ import express from "express";
 
 import HttpException from "../exceptions/Http.exception";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+function getAllConstraints(errors: ValidationError[]): string[] {
+    const constraints: string[] = [];
+    for (const error of errors) {
+        if (error.constraints) {
+            const constraintValues = Object.values(error.constraints);
+            constraints.push(...constraintValues);
+        }
+        if (error.children) {
+            const childConstraints = getAllConstraints(error.children);
+            constraints.push(...childConstraints);
+        }
+    }
+    return constraints;
+}
+
 export default function validationMiddleware(type: any, skipMissingProp = false): express.RequestHandler {
     return (req, res, next) => {
-        // forbidUnknownValues: false
-        // const validatorObject = Object.assign(new type(), req.body);
         validate(plainToInstance(type, req.body), { skipMissingProperties: skipMissingProp }).then((errors: ValidationError[]) => {
             if (errors!.length > 0) {
-                // Break down, if validate nested object in latest version of class-validator
-                // const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(", ");
-                const message: string[] = [];
-                for (let i = 0; i < errors.length; i++) {
-                    if (errors[i].constraints) {
-                        message.push(Object.values(errors[i].constraints).join(", "));
-                    }
-                    if (errors[i].children!.length > 0) {
-                        for (let j = 0; j < errors[i].children.length; j++) {
-                            if (errors[i].children[j].constraints) {
-                                message.push(Object.values(errors[i].children[j].constraints).join(", "));
-                            }
-                        }
-                    }
-                }
-                next(new HttpException(400, "DTO error: " + message.join("; ")));
+                next(new HttpException(400, "DTO error(s): " + getAllConstraints(errors).join(", ")));
             } else {
                 next();
             }

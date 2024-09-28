6ef032662be565e495a5557aec603ff08557da73
nits68 <nits.laszlo@jedlik.eu>
2024-09-28 19:14:22 +0200

    netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,32 +1,26 @@
-import express, { Router } from "express";
+// import { Router } from "express";
 import serverless from "serverless-http";
 
-const api = express();
+// const api = express();
+// const router = Router();
+// router.get("/hello", (req, res) => res.send("Hello World!"));
+// api.use("/api/", router);
+// export const handler = serverless(api);
+import App from "../../src/_app";
+import AuthenticationController from "../../src/authentication/_authentication.controller";
+import AuthorController from "../../src/authentication/_authentication.controller";
+import PostController from "../../src/post/_post.controller";
+import RecipeController from "../../src/recipe/_recipe.controller";
+import ReportController from "../../src/report/_report.controller";
+import UserController from "../../src/user/_user.controller";
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+const api: App = new App([
+    new AuthenticationController(),
+    new UserController(),
+    new PostController(),
+    new RecipeController(),
+    new AuthorController(),
+    new ReportController(),
+]);
 
-api.use("/api/", router);
-
-export const handler = serverless(api);
-
-// import serverless from "serverless-http";
-
-// import App from "../../src/app";
-// import AuthenticationController from "../../src/authentication/authentication.controller";
-// import AuthorController from "../../src/author/author.controller";
-// import PostController from "../../src/post/post.controller";
-// import RecipeController from "../../src/recipe/recipe.controller";
-// import ReportController from "../../src/report/report.controller";
-// import UserController from "../../src/user/user.controller";
-
-// const api: App = new App([
-//     new AuthenticationController(),
-//     new UserController(),
-//     new PostController(),
-//     new RecipeController(),
-//     new AuthorController(),
-//     new ReportController(),
-// ]);
-
-// export const handler = serverless(api.getServer());
+export const handler = serverless(api.getServer());

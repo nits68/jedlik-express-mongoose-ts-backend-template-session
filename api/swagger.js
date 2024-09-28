1087e7157e631b1d9e6238c4f796d85219bff7e1
nits68 <nits.laszlo@jedlik.eu>
2024-09-28 16:45:41 +0200

    up666

--- api/swagger.js
+++ api/swagger.js
@@ -8,7 +8,7 @@ const options = {
     definition: {
         openapi: "3.1.0",
         info: {
-            title: "Jedlik API docs",
+            title: "Jedlik API docs 123",
             version: "0.0.1",
             description: "<img alt='DB_diagram' height='700px' src='https://nits68.github.io/static/session/db_diagram_new_new.jpg' />",
         },
@@ -18,7 +18,7 @@ const options = {
             },
         ],
     },
-    apis: [`${__dirname}/**/*.{dto,controller,model,exception}.{ts,js,yml}`],
+    apis: [`${__dirname}/**/*.{mod,cont,dto,controller,model,exception}.{ts,js,yml}`],
 };
 exports.default = (0, swagger_jsdoc_1.default)(options);
 //# sourceMappingURL=swagger.js.map
